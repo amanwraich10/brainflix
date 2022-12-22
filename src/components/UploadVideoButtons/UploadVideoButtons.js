ab55f6887c401a87661a49a40412b55e450e508f
Amandeep Kaur <deepamankaur97@gmail.com>
2022-12-22 10:58:33 -0500

    Link the publish button to home page

--- src/components/UploadVideoButtons/UploadVideoButtons.js
+++ src/components/UploadVideoButtons/UploadVideoButtons.js
@@ -1,10 +1,14 @@
 import "../UploadVideoButtons/UploadVideoButtons.scss";
+import { Link } from "react-router-dom";
 
 function UploadVideoButtons() {
 	return (
 		<>
 			<hr className="uploadform__linebreak"></hr>
-			<button className="uploadform__button--publish">PUBLISH</button>
+			<Link to="/">
+				<button className="uploadform__button--publish">PUBLISH</button>
+			</Link>
+
 			<button className="uploadform__button--cancel">CANCEL</button>
 		</>
 	);
